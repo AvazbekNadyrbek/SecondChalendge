97a53223a0daad374bfdcc001978e44bd8172cc2
AvazbekNadyrbek <spatium.avazbek@gmail.com>
2025-03-20 21:29:48 +0100

    Finished the Chalange! Learn how to use toggle logic with ifesle structure!

--- SecondChalendge/ContentView.swift
+++ SecondChalendge/ContentView.swift
@@ -8,12 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var text = ""
+    @State private var imageSystem = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            Image(systemName: "\(imageSystem)")
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.orange)
+                .frame(width: 300, height: 300)
+            Text("\(text)")
+                .font(.largeTitle)
+                .foregroundStyle(.black)
+                .padding()
+            Spacer()
+            Button("Press me!") {
+                
+                let messsage1 = "You are Awesome!!!"
+                let messsage2 = "You are Greate!!!!"
+                
+                let imageString = ["hand.thumbsup", "sun.max.fill"]
+                
+                
+                if text == messsage1 {
+                    text = messsage2
+                    imageSystem = imageString[0]
+                } else {
+                    text = messsage1
+                    
+                    imageSystem = imageString[1]
+                }
+                
+            }
+            .buttonStyle(.borderedProminent)
+            .font(.headline)
+            .tint(.orange)
+            
         }
         .padding()
     }
