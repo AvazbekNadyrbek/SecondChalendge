d8eb5c5d5899ec315b4bf4d8ed3ee9847fb4c51c
AvazbekNadyrbek <spatium.avazbek@gmail.com>
2025-03-20 20:50:23 +0100

    Initial Commit

---
+++ SecondChalendge/SecondChalendgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecondChalendgeApp.swift
+//  SecondChalendge
+//
+//  Created by Авазбек Надырбек уулу on 20.03.25.
+//
+
+import SwiftUI
+
+@main
+struct SecondChalendgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
